b29d2f7aacacf272c3a6f09f37866eae612fd398
DimitarZlatev10 <aguerofc@abv.bg>
2023-02-06 17:43:46 +0200

    created styled and added functionality to slider

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('aa');
+const wrapper = document.querySelector(".sliderWrapper");
+const menuItems = document.querySelectorAll(".menuItem");
+
+menuItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    console.log("You clicked" + index);
+    wrapper.style.transform = `translateX(${-100 * index}vw)`;
+  });
+});
